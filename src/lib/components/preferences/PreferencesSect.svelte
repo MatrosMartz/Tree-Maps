59d2472c401e756a92ffe9779baf9f8e705e0416
TsonkoAltCntrl <matrosmartz@gmal.com>
2022-04-13 13:43:36 -0500

    add set cookies botton

--- src/lib/components/preferences/PreferencesSect.svelte
+++ src/lib/components/preferences/PreferencesSect.svelte
@@ -7,22 +7,39 @@
 	import RadioColors from './RadioColors.svelte';
 	import SwitchBtn from './SwitchBtn.svelte';
 
+	import panelStore from '../../stores/panel';
+	import alertStore from '../../stores/alert';
+
 	export let active: boolean;
+
+	function onClick() {
+		panelStore.setToNone();
+		alertStore.set_cookies();
+	}
 </script>
 
 <section class="panel-sect right" class:active>
 	<h5 class="sect-h5">Preferencias</h5>
+
 	<SwitchBtn id="theme">
 		<legend slot="name">tema</legend>
 		<LightmodeIcon slot="first" />
 		<DarkmodeIcon slot="last" />
 	</SwitchBtn>
-
+	
 	<SwitchBtn id="animation">
 		<legend slot="name">animaciones</legend>
 		<NonAnimationIcon slot="first" />
 		<AnimationIcon slot="last" />
 	</SwitchBtn>
-
+	
 	<RadioColors />
+
+	<button type="button" class="terciary-btn" on:click={onClick}>modificar las cookies</button>
 </section>
+
+<style>
+	.terciary-btn {
+		font-weight: 500;
+	}
+</style>
