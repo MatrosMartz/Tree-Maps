213d94fde1ff7008a0dd5448201a32ec2f4ba13f
TsonkoAltCntrl <matrosmartz@gmal.com>
2022-05-12 16:03:08 -0500

    refactor for conection with database

---
+++ src/lib/database/species.ts
@@ -0,0 +1,17 @@
+import type { FilterSpecie } from '../types/database';
+
+import type { Specie } from '../types/database/models';
+
+import db from './conection';
+
+export async function getManySpecies(filter?: FilterSpecie) {
+	const collection = (await db()).collection('species');
+
+	return <Specie[]>(await collection.find(filter).toArray());
+}
+
+export async function getOneSpecies(filter?: FilterSpecie) {
+	const collection = (await db()).collection('species');
+
+	return <Specie>(await collection.findOne(filter));
+}
