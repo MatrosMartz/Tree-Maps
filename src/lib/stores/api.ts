8ee785d18a277b61978f18ac8d4ba57d839ac805
TsonkoAltCntrl <matrosmartz@gmal.com>
2022-05-16 20:49:36 -0500

    change api :D

--- src/lib/stores/api.ts
+++ src/lib/stores/api.ts
@@ -1,3 +1,5 @@
+import type { TimeoutID } from '$lib/types';
+
 import type { Specie } from '$lib/types/database/models';
 
 import { browser } from '$app/env';
@@ -5,6 +7,8 @@ import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
 export function getSpecies() {
+	let timeoutSearch: TimeoutID;
+
 	const species = writable<Specie[]>([]);
 	let anySpecies: Specie[] = [];
 
@@ -28,7 +32,68 @@ export function getSpecies() {
 	}
 
 	function searchSpecies(value: string) {
-		species.set(anySpecies?.filter(s => s.cientificName.toLowerCase().includes(value)));
+		clearTimeout(timeoutSearch);
+		timeoutSearch = setTimeout(() => {
+			species.set(
+				anySpecies?.filter(s =>
+					s.cientificName.toLowerCase().replace('-', ' ').includes(value)
+				)
+			);
+		}, 500);
+	}
+
+	return {
+		species,
+		fetchSpecies,
+		searchSpecies,
+	};
+}
+
+export function getSpeciesNames() {
+	let timeoutSearch: TimeoutID;
+
+	const species = writable<{ name: string; cientificName: string; visible: boolean }[]>([]);
+	let anySpecies: { name: string; cientificName: string }[] = [];
+
+	async function fetchSpecies() {
+		if (browser) {
+			try {
+				const res = await fetch('/api/species', {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				});
+
+				anySpecies = (<Specie[]>await res.json()).map(({ name, cientificName }) => {
+					return { name, cientificName };
+				});
+
+				species.set(
+					anySpecies.map(ss => {
+						return { ...ss, visible: false };
+					})
+				);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	}
+
+	function searchSpecies(value: string) {
+		clearTimeout(timeoutSearch);
+		timeoutSearch = setTimeout(() => {
+			species.update(ss => {
+				return ss.map(s => {
+					return {
+						...s,
+						visible: (
+							s.cientificName.toLowerCase().replace('-', ' ') + s.name.toLowerCase()
+						).includes(value),
+					};
+				});
+			});
+		}, 500);
 	}
 
 	return {
